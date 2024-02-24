9361062b4d4d05b139490653dca73d30de8df624
Jonathan White <jawh@umich.edu>
2024-02-23 19:21:27 -0700

    Moved more functions to test_setup.py module.

--- curve_refitting.py
+++ curve_refitting.py
@@ -23,13 +23,13 @@ def main():
 
   # initialize pos and vel functions
   init_pos = lambda: np.array([random.uniform(0.0, frame_width), random.uniform(0.0, frame_width)])
-  init_vel = lambda: polar_to_cartesian([random.uniform(min_vel, max_vel), random.uniform(0, 2 * np.pi)])
+  init_vel = lambda: setup.polar_to_cartesian([random.uniform(min_vel, max_vel), random.uniform(0, 2 * np.pi)])
 
   # simulate trajectories
-  curves = [setup.integrate_euler(init_pos(), init_vel(), brownian_acc(brownian_sigma), dt, max_time) for _ in np.arange(n_curves)]
+  curves = [setup.integrate_euler(init_pos(), init_vel(), setup.brownian_acc(brownian_sigma), dt, max_time) for _ in np.arange(n_curves)]
 
   # tag individual states with curve id for validation
-  curves = id_curves(curves)
+  curves = setup.id_curves(curves)
 
   # split the curves
   #curves = split_curves(curves, gap_portion)
@@ -69,15 +69,6 @@ def main():
   plt.xlim(plot_min, plot_max) # add some padding
   plt.ylim(plot_min, plot_max)
   plt.show()
-
-def brownian_acc(sigma=1.0):
-  return lambda: np.array([random.gauss(0, sigma) for _ in range(2)])
-
-def polar_to_cartesian(polar):
-  return np.array([polar[0] * np.cos(polar[1]), polar[0] * np.sin(polar[1])])
-
-def id_curves(curves):
-  return [[np.hstack([state, i]) for state in curve] for i, curve in enumerate(curves)] # id is last element
 
 def split_curves(curves, gap_portion):
   result = []
--- test_setup.py
+++ test_setup.py
@@ -1,6 +1,8 @@
 import numpy as np
+import random
 
 def integrate_euler(pos, vel, acc, dt, max_time):
+  """Use Euler's method to generate curves given acceleration function."""
   t = 0.0
   trajectory = [np.hstack([t, pos])]
   for t in np.arange(dt, max_time, dt):
@@ -8,3 +10,15 @@ def integrate_euler(pos, vel, acc, dt, max_time):
     vel += acc() * dt
     trajectory.append(np.hstack([t, pos]))
   return trajectory
+
+def id_curves(curves):
+  """Tag individual states with curve id for validation."""
+  return [[np.hstack([state, i]) for state in curve] for i, curve in enumerate(curves)] # id is last element
+
+def brownian_acc(sigma=1.0):
+  """Simulate random (Brownian) motion."""
+  return lambda: np.array([random.gauss(0, sigma) for _ in range(2)])
+
+def polar_to_cartesian(polar):
+  """Convert polar coordinates to Cartesian coordinates."""
+  return np.array([polar[0] * np.cos(polar[1]), polar[0] * np.sin(polar[1])])
